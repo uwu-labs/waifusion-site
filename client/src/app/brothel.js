461757a5ef5e3fea39209f539304718e153e1ce4
Chase Manning <me@chasemanning.co.nz>
2021-03-05 21:21:56 +1300

    :rocket: add placeholder brothel page

---
+++ client/src/app/brothel.js
@@ -0,0 +1,38 @@
+// Frameworks
+import React, { useContext } from "react";
+import { navigate } from "gatsby";
+import {
+  Heading,
+  Blockie,
+  Flex,
+  EthAddress,
+  Button,
+  Text,
+  Box,
+} from "rimble-ui";
+
+// Wallet Interface
+import Wallet from "./wallets";
+
+// Data Store
+import { RootStoreContext } from "./stores/root.store";
+
+// Main Route
+const Provenance = (props) => {
+  const rootStore = useContext(RootStoreContext);
+  const { walletStore } = rootStore;
+
+  return (
+    <>
+      <Heading as={"h3"}>&nbsp;&nbsp;List</Heading>
+
+      <Box mt={20}>
+        <Flex>
+          <Text>No items currently</Text>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default Provenance;
