0442552840aedb2834e1e62ed25233deaf0ba431
Chase Manning <me@chasemanning.co.nz>
2021-03-06 08:43:57 +1300

    :art: change maid cafe to use box styles

--- client/src/pages/maidcafe.js
+++ client/src/pages/maidcafe.js
@@ -7,6 +7,7 @@ import OverviewGreenBar from "../images/overview_green_bar.png";
 import WaifuPinkBar from "../images/waifucard_pink_bar.png";
 // import { GLOBALS } from "../app/utils/globals.js";
 import styled from "styled-components";
+import { BoxUpper, BoxContent, Header, Content } from "../styles/BoxContent";
 
 const PageContainer = styled.div`
   width: 100%;
@@ -21,62 +22,6 @@ const PurchaseOptionsContainer = styled.div`
   margin-top: 60px;
   justify-content: space-evenly;
   max-width: 1435px;
-`;
-
-const BoxUpper = styled.div`
-  height: 100%;
-  margin-top: 2px;
-  margin-left: 2px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 549px;
-`;
-
-const BoxContent = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: rgba(
-    24.000000469386578,
-    20.000000707805157,
-    37.00000159442425,
-    1
-  );
-  padding-left: 62px;
-  padding-right: 62px;
-  padding-bottom: 20px;
-  margin-top: 2px;
-  margin-bottom: 4px;
-  margin-right: 4px;
-  margin-left: 2px;
-  display: flex;
-  flex-direction: column;
-  color: #fff;
-  max-width: 1435px;
-`;
-
-const Header = styled.h3`
-  color: red;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  color: rgba(252.00000017881393, 116.00000068545341, 198.00000339746475, 1);
-  padding-top: 10px;
-  font-family: VT323;
-  font-size: 42px;
-  letter-spacing: 0;
-  line-height: undefined;
-  font-weight: normal;
-`;
-
-const Content = styled.p`
-  width: 100%;
-  font-family: VT323 !important;
-  font-style: normal !important;
-  font-weight: normal !important;
-  font-size: 24px !important;
-  line-height: 24px !important;
-  text-align: center !important;
 `;
 
 const ButtonContainer = styled.div`
