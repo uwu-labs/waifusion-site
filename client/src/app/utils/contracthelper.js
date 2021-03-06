e05d206a7fbab7dfaa8a0445ec22106eff159623
Chase Manning <me@chasemanning.co.nz>
2021-03-06 14:27:31 +1300

    :sparkles: add get dungeon contract function

--- client/src/app/utils/contracthelper.js
+++ client/src/app/utils/contracthelper.js
@@ -16,6 +16,24 @@ const dungeonContract = new web3.eth.Contract(
   GLOBALS.DUNGEON_CONTRACT_ADDRESS
 );
 
+// Dungeon Functions
+const getDungeonContract = async () => {
+  const defaultAccount = await ethEnabled();
+
+  if (defaultAccount === false) {
+    return false;
+  }
+
+  return new window.web3.eth.Contract(
+    DUNGEON,
+    GLOBALS.DUNGEON_CONTRACT_ADDRESS,
+    {
+      from: defaultAccount,
+    }
+  );
+};
+
+// Other Functions
 const ethEnabled = async () => {
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
@@ -191,6 +209,7 @@ const getTimestampFromBlock = async (hash) => {
 export {
   web3,
   contract,
+  getDungeonContract,
   ethEnabled,
   getAllowance,
   getWETContract,
