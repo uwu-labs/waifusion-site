9273d71642cb93feb92b5cca49e6bfdff967283f
Chase Manning <me@chasemanning.co.nz>
2021-03-07 14:44:45 +1300

    :rotating_light: fix wallet store lint warnings

--- client/src/app/stores/wallet.store.js
+++ client/src/app/stores/wallet.store.js
@@ -1,65 +1,41 @@
-import { decorate, observable, computed, action, makeAutoObservable } from 'mobx';
-import { create, persist } from 'mobx-persist'
-import * as _ from 'lodash';
-import { autoSave } from '../utils/autoSave';
-
-
-// export class WalletStore {
-//     type = "";
-//     defaultAddress = "";
-    
-//     hasAccount() {
-//         return !_.isEmpty(this.defaultAddress)
-//     }
-// }
-
-// decorate(WalletStore, {
-//     type: [persist,observable],
-//     defaultAddress: [persist,observable],
-//     hasAccount: computed
-// });
+import { decorate, observable, computed } from "mobx";
+import * as _ from "lodash";
+import { autoSave } from "../utils/autoSave";
 
 class WalletStore {
-    type = '';
-    defaultAddress = '';
-    isWalletConnected = false;
-    web3 = null;
-    isPendingApproval = false;
-    isApproved = false;
+  type = "";
+  defaultAddress = "";
+  isWalletConnected = false;
+  web3 = null;
+  isPendingApproval = false;
+  isApproved = false;
 
-    
-    
-    constructor(rootStore) {
-        //makeAutoObservable(this);
-        this.type = '';
-        this.defaultAddress ='';
-        this.isWalletConnected = false;
-        this.web3 = null;
-        this.isPendingApproval = false;
-        this.isApproved = false;
-        this.rootStore = rootStore;
-        
-        autoSave(this, 'walletStore');
-        console.log("Wallet Store Contructor");
-    }
+  constructor(rootStore) {
+    this.type = "";
+    this.defaultAddress = "";
+    this.isWalletConnected = false;
+    this.web3 = null;
+    this.isPendingApproval = false;
+    this.isApproved = false;
+    this.rootStore = rootStore;
 
-    get hasAccount() {
-        return !_.isEmpty(this.defaultAddress)
-    }
+    autoSave(this, "walletStore");
+    console.log("Wallet Store Contructor");
+  }
 
+  get hasAccount() {
+    return !_.isEmpty(this.defaultAddress);
+  }
 }
 decorate(WalletStore, {
-    defaultAddress: observable,
-    hasAccount: computed,
-    contract: observable,
-    wetContract: observable,
-    web3: observable,
-    isPendingApproval: observable,
-    isApproved: observable,
-    isWalletConnected: observable
-    // walletWeb: observable,
-    // walletInstance: observable
-
+  defaultAddress: observable,
+  hasAccount: computed,
+  contract: observable,
+  wetContract: observable,
+  web3: observable,
+  isPendingApproval: observable,
+  isApproved: observable,
+  isWalletConnected: observable,
 });
 
 export default WalletStore;
