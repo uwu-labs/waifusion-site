ece672b64251bd900830c8a8e1e2f862b8d86285
Chase Manning <me@chasemanning.co.nz>
2021-03-07 14:46:08 +1300

    :rotating_light: fix root store lint warnings

--- client/src/app/stores/root.store.js
+++ client/src/app/stores/root.store.js
@@ -1,34 +1,26 @@
-import { createContext } from 'react';
-import { create } from "mobx-persist";
+import { createContext } from "react";
 
-import WalletStore from './wallet.store';
-import WETStore from './wet.store';
-import TransactionStore from './transactions.store';
-import DetailStore from './detail.store';
-import GalleryStore from './gallery.store';
-import { observable, action, decorate, makeAutoObservable } from 'mobx';
-
-import autoSave from '../utils/autoSave';
+import WalletStore from "./wallet.store";
+import WETStore from "./wet.store";
+import TransactionStore from "./transactions.store";
+import DetailStore from "./detail.store";
+import GalleryStore from "./gallery.store";
+import { observable, decorate } from "mobx";
 
 class RootStore {
-    //makeAutoObservable(this);
-    walletStore = new WalletStore(this);
-    WETStore = new WETStore(this);
-    transactionStore = new TransactionStore(this);
-    detailStore = new DetailStore(this);
-    galleryStore = new GalleryStore(this);
-
+  walletStore = new WalletStore(this);
+  WETStore = new WETStore(this);
+  transactionStore = new TransactionStore(this);
+  detailStore = new DetailStore(this);
+  galleryStore = new GalleryStore(this);
 }
 
 decorate(RootStore, {
-    walletStore: observable, 
-    WETStore: observable,
-    transactionStore: observable, 
-    detailStore: observable,
-    galleryStore: observable
-})
-
+  walletStore: observable,
+  WETStore: observable,
+  transactionStore: observable,
+  detailStore: observable,
+  galleryStore: observable,
+});
 
 export const RootStoreContext = createContext(new RootStore());
-
-
