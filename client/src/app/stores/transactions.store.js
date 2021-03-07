a817be6b2df46fd25ba7aa74612e0cd9d871d3c9
Chase Manning <me@chasemanning.co.nz>
2021-03-07 14:45:35 +1300

    :rotating_light: fix transaction store lint warnings

--- client/src/app/stores/transactions.store.js
+++ client/src/app/stores/transactions.store.js
@@ -1,98 +1,32 @@
-import { decorate, observable, computed, action } from 'mobx';
-import * as _ from 'lodash';
-
-
-// export class TransactionStore {
-//     pendingTransactions = [{
-//         transaction: {}
-//     }];
-//     completedTransactions = [{
-//         transaction: {}
-//     }];
-
-
-
-//     addPendingTransaction({txHash, description}) {
-//         const transDetail = {
-//             txHash,
-//             description,
-//             status: 0
-//         }
-//         this.pendingTransactions.push(transDetail);
-//     }
-
-//     addCompletedTransaction({ txDetails, status }) {
-//         this.pendingTransactions = this.pendingTransactions.filter(item => item.txHash !== txDetails.txHash);
-//         if (status === true) {
-//             // Vue.noty.success(
-//             //   "<img src='/images/success-icon.png' style='width: 40px;'> Transaction successful for " +
-//             //     txDetails.description,
-//             // );
-//             // const audio = new Audio('/sound/swiftly.mp3');
-//             // audio.play();
-//           } else {
-//             // Vue.noty.error(
-//             //   "<img src='/images/error-soft-icon.png' style='width: 40px;'> Transaction reverted for " +
-//             //     txDetails.description,
-//             // );
-//           }
-//           this.completedTransactions.push(txDetails);
-//     }
-
-
-
-// }
-
-// decorate(TransactionStore, {
-//     pendingTransactions: observable,
-//     completedTransactions: observable
-// });
-
-
-
-
+import { decorate, observable } from "mobx";
 
 class TransactionStore {
+  pendingTransactions = [];
+  completedTransactions = [];
 
-    pendingTransactions = [];
-    completedTransactions = [];
-    
+  constructor(rootStore) {
+    this.rootStore = rootStore;
+  }
 
-    constructor(rootStore) {
-        this.rootStore = rootStore;
-    }
+  addPendingTransaction({ txHash, description }) {
+    const transDetail = {
+      txHash,
+      description,
+      status: 0,
+    };
+    this.pendingTransactions.push(transDetail);
+  }
 
-    addPendingTransaction({txHash, description}) {
-        const transDetail = {
-            txHash,
-            description,
-            status: 0
-        }
-        this.pendingTransactions.push(transDetail);
-    }
-
-    addCompletedTransaction({ txDetails, status }) {
-        this.pendingTransactions = this.pendingTransactions.filter(item => item.txHash !== txDetails.txHash);
-        if (status === true) {
-            // Vue.noty.success(
-            //   "<img src='/images/success-icon.png' style='width: 40px;'> Transaction successful for " +
-            //     txDetails.description,
-            // );
-            // const audio = new Audio('/sound/swiftly.mp3');
-            // audio.play();
-          } else {
-            // Vue.noty.error(
-            //   "<img src='/images/error-soft-icon.png' style='width: 40px;'> Transaction reverted for " +
-            //     txDetails.description,
-            // );
-          }
-          this.completedTransactions.push(txDetails);
-    }
+  addCompletedTransaction({ txDetails, status }) {
+    this.pendingTransactions = this.pendingTransactions.filter(
+      (item) => item.txHash !== txDetails.txHash
+    );
+    this.completedTransactions.push(txDetails);
+  }
 }
 decorate(TransactionStore, {
-
-    pendingTransactions: observable,
-    completedTransactions: observable
+  pendingTransactions: observable,
+  completedTransactions: observable,
 });
 
 export default TransactionStore;
