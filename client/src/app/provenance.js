c9bd6597d090c2b1ef6fd8bc132733c971cd0a58
Chase Manning <me@chasemanning.co.nz>
2021-03-07 13:52:28 +1300

    :rotating_light: fix prevenance lint warnings

--- client/src/app/provenance.js
+++ client/src/app/provenance.js
@@ -1,33 +1,20 @@
 // Frameworks
-import React, { useContext } from 'react';
-import { navigate } from 'gatsby';
-import { Heading, Blockie, Flex,EthAddress, Button, Text, Box } from 'rimble-ui';
-
-// Wallet Interface
-import Wallet from './wallets';
-
-// Data Store
-import { RootStoreContext } from './stores/root.store';
+import React from "react";
+import { Heading, Flex, Text, Box } from "rimble-ui";
 
 // Main Route
-const Provenance = (props) => {
-    const rootStore = useContext(RootStoreContext);
-    const { walletStore } = rootStore;
+const Provenance = () => {
+  return (
+    <>
+      <Heading as={"h3"}>&nbsp;&nbsp;List</Heading>
 
-    return (
-        <>
-            <Heading as={"h3"}>
-                &nbsp;&nbsp;List
-            </Heading>
-
-            <Box mt={20}>
-                <Flex>
-                   <Text>No items currently</Text>
-                </Flex>
-            </Box> 
-
-        </>
-    );
-}
+      <Box mt={20}>
+        <Flex>
+          <Text>No items currently</Text>
+        </Flex>
+      </Box>
+    </>
+  );
+};
 
 export default Provenance;
