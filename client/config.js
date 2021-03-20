97d991d1858fd53bfaf2bdf86f39dd64dadfddb9
Phineas <phin@phineas.io>
2021-03-20 16:39:52 +0000

    Fix manifest

--- client/config.js
+++ client/config.js
@@ -1,19 +1,17 @@
 module.exports = {
-    siteTitle: 'WaiFusion',
-    siteDesc: 'Ethereum Dapps using Web3',
-    siteAuthor: 'WaiFusion Team',
+    siteTitle: 'Waifusion',
+    siteDesc: 'Waifusion is a digital Waifu collection of 16,384 guaranteed-unique Waifusion NFTs.',
+    siteAuthor: 'Waifusion Team',
     siteLogoUrl: 'src/images/logo_icon.png',
 
-    manifestName: 'Ethereum Dapp',
-    manifestShortName: 'EthDapp', // max 12 characters
-    manifestStartUrl: 'https://gatsby-eth-dapp.runkodapps.com/',
+    manifestName: 'Waifusion',
+    manifestShortName: 'Waifusion', // max 12 characters
+    manifestStartUrl: 'https://waifusion.sexy',
     manifestBackgroundColor: '#663399',
     manifestThemeColor: '#663399',
     manifestDisplay: 'standalone',
     manifestIcon: 'src/images/logo_icon.png',
 
-    // This path is subpath of your hosting https://your.domain/gatsby-eth-dapp-starter/
-    // pathPrefix: `/gatsby-eth-dapp-starter/`,
     pathPrefix: '/',
 
     // social
@@ -21,12 +19,12 @@ module.exports = {
         {
             icon: 'fa-twitter',
             name: 'Twitter',
-            url: 'https://twitter.com/[__your_social_link__]',
+            url: 'https://twitter.com/waifusion',
         },
         {
             icon: 'fa-envelope-o',
             name: 'Email',
-            url: 'mailto:[__your_email_address__]',
+            url: 'mailto:waifusion@protonmail.com',
         },
     ],
 };
