164e12e18c209f04131f40230e6addfee39c689a
waifu-lover <>
2021-03-21 19:09:09 +1100

    Display revealed waifus in row; add loading state; show max 3 revealed waifus; display number of waifus revealed

--- client/src/components/revealComplete.js
+++ client/src/components/revealComplete.js
@@ -1,15 +1,20 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { observer } from 'mobx-react-lite';
 
 import styled from "styled-components";
 import Popup from "./popup";
 import { BoxContent, Header } from "../styles/BoxContent";
-import { Button } from "rimble-ui";
+import { Button, Loader } from "rimble-ui";
 import WaifuCard from "./waifuCard.js";
 
 import { RootStoreContext } from "../app/stores/root.store";
 
 const StyledRevealComplete = styled.div``;
+
+const RevealWaifusContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const ButtonContainer = styled.div`
   width: 100%;
@@ -21,22 +26,33 @@ const ButtonContainer = styled.div`
 const getRevealedWaifus = async (WETStore) => {
   const oldWaifuIds = WETStore.ownedItems.map(item => item.id);
   await WETStore.syncOwnedItems();
+  const newWaifuIds = WETStore.ownedItems.map(item => item.id);
   const newWaifus = WETStore.ownedItems;
   return newWaifus.filter(waifu => !oldWaifuIds.includes(waifu.id));
+  return newWaifuIds.filter(newId => !oldWaifuIds.includes(newId));
 }
 
 const RevealComplete = observer(({ show }) => {
   const rootStore = useContext(RootStoreContext);
   const { WETStore } = rootStore;
-  const [revealedWaifus, setRevealedWaifus] = useState([])
+  const [revealedWaifuIds, setRevealedWaifuIds] = useState([]);
+  const [loading, setLoading] = useState(false);
   
-  useEffect(() => {
-    if (show) {
-      getRevealedWaifus(WETStore).then((value) => {
-        setRevealedWaifus(value);
-      })
-    }
-  })
+  useEffect(
+    () => {
+      if (show) {
+        setLoading(true);
+        getRevealedWaifus(WETStore)
+        .then((value) => {
+          setRevealedWaifuIds(value);
+          setLoading(false);
+        }).catch((err) => {
+          setLoading(false);
+        });
+      }
+    },
+    [show, WETStore]
+  )
 
   return (
     <StyledRevealComplete>
@@ -44,24 +60,38 @@ const RevealComplete = observer(({ show }) => {
         show={show}
         content={
           <BoxContent>
-            <Header>Waifu Revealed!!!</Header>
             {
-              show && (
-                revealedWaifus.map((item) => {
-                  return <WaifuCard cardIndex={item.id} />
-                })
-              )
+              loading 
+                ? 
+                  <Loader
+                    className="wallet-waifu-loader"
+                    color="#FBE55F"
+                    size="80px"
+                  />
+                :
+              <>
+                <Header>{revealedWaifuIds.length} Waifu{revealedWaifuIds.length > 1 && 's'} Revealed!!!</Header>
+                <RevealWaifusContainer>
+                  {
+                    show && (
+                      revealedWaifuIds.map((id) => {
+                        return <WaifuCard cardIndex={id} key={id} />
+                      }).slice(0, 3)
+                    )
+                  }
+                </RevealWaifusContainer>
+                <ButtonContainer>
+                  <Button.Outline
+                    className="waifu-card-buttons"
+                    onClick={() => {
+                      window.location.href = "/app";
+                    }}
+                  >
+                    <span className="waifu-button-learnmore">View Waifus</span>
+                  </Button.Outline>
+                </ButtonContainer>
+              </>
             }
-            <ButtonContainer>
-              <Button.Outline
-                className="waifu-card-buttons"
-                onClick={() => {
-                  window.location.href = "/app";
-                }}
-              >
-                <span className="waifu-button-learnmore">View Waifus</span>
-              </Button.Outline>
-            </ButtonContainer>
           </BoxContent>
         }
       />
