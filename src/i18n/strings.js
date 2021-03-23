34cc9d38f701399e64a70194c1b342a69e0b1b52
Phineas <phin@phineas.io>
2021-03-23 03:27:41 +0000

    Start on adding strings for translation purposes

---
+++ src/i18n/strings.js
@@ -0,0 +1,51 @@
+/**
+ * This file contains the i18n translations for the Waifusion project
+ * If a value is null, it'll default to the en version
+ */
+const strings = {
+  "en": {
+    NAME: "Waifusion",
+    PAGE_HOME: "Home",
+    PAGE_OVERVIEW: "Overview",
+    PAGE_GALLERY: "Gallery",
+    PAGE_WET: "WET Token",
+    PAGE_WALLET: "My Wallet",
+    PAGE_HISTORY: "My History",
+    PAGE_PROVENANCE: "Provenance",
+    PAGE_DUNGEON: "Dungeon",
+    WAIFU_NAME_PREFIX: "Waifu Name: ",
+    WAIFU_NAME_EXAMPLE: "Waifu Name #6969",
+    WAIFUSION_DESCRIPTION_INTRO: "Waifusion is a digital Waifu collection. There are 16,384 guaranteed-unique Waifusion NFTs. They’re just like you; a beautiful work of art, but 2-D and therefore, superior, Anon-kun.",
+    WAIFUSION_DESCRIPTION_1: "Waifusions are randomly and fairly distributed on-chain which means they don’t like it when perverts try to add too many Waifus to their harem. Be faithful to your Waifus and only buy a max of 20 at once for your harem!",
+    WAIFUSION_DESCRIPTION_2: " Each Waifu is wholly unique and yours forever... unless you sell them... Baka.",
+    GET_WAIFUS_WET_HEADER: "Get Your Waifus WET?",
+    WAIFUSION_WET_DESCRIPTION: "When you hold a Waifusion NFT, you get WET. WET lets you change the name of your currently-owned Waifusions. After 10 years, no one gets WET anymore. Ben Shapiro was right.",
+    LEARN_MORE_BUTTON: "Learn MORE",
+    WAIFUSION_DUNGEON_TITLE: "The Dungeon",
+    WAIFUSION_DUNGEON_DESCRIPTION_INTRO: "Help! All the remaining waifus have been thrown in the dungeon!",
+    WAIFUSION_DUNGEON_DESCRIPTION_1: "You can buy a waifu's freedom by paying the last presale price of 0.7ETH",
+    WAIFUSION_DUNGEON_DESCRIPTION_2: "Have a naughty waifu? Burn WET to throw her into the dungeon permanently and exchange her for a more obedient waifu.",
+    ENTER_DUNGEON_BUTTON: "To the Dungeon...",
+    CONNECT_WALLET_HEADER: "Connect your Wallet",
+    CONNECT_WALLET_BUTTON: "Connect",
+    NO_ETH_WALLET_DETECTED: "No Ethereum Wallet Detected",
+    PREVIOUS_BUTTON: "Previous",
+    NEXT_BUTTON: "Next",
+    BUY_WAIFUS_HEADER: "Select Amount to Buy",
+    WAIFUS_REVEAL_WARNING: "After {type}, you have 45 minutes to reveal or Waifus will be lost forever. If gas fees show as high, try again in 1  minute.",
+    WAIFUS_REVEAL_COMPLETE: "Waifus Revealed!",
+    REVEAL_WAIFUS_BUTTON: "Reveal Waifus",
+    VIEW_WAIFUS_BUTTON: "View Waifus",
+    SELECT_WAIFUS_TO_BURN_HEADER: "Add Waifus to Burn",
+    BURN_WAIFUS_BUTTON: "Burn Waifus",
+  },
+  "jp": {
+    ...strings.en,
+    NAME: "Waifusion",
+    BUY_WAIFUS_HEADER: null,
+    WAIFUS_REVEAL_COMPLETE: null,
+    VIEW_WAIFUS_BUTTON: null
+  }
+}
+
+export default strings;
