3a2283976598f1be83fdd9160258d19c27128bf8
Chase Manning <me@chasemanning.co.nz>
2021-03-24 17:13:29 +1300

    :fire: remove redundant console log

--- src/app/components/PrivateRoute.js
+++ src/app/components/PrivateRoute.js
@@ -1,25 +1,21 @@
-import React, { useContext, useEffect } from 'react';
-import {navigate} from 'gatsby';
-import {observer} from 'mobx-react-lite';
+import React, { useContext, useEffect } from "react";
+import { navigate } from "gatsby";
+import { observer } from "mobx-react-lite";
 
-import { RootStoreContext } from '../stores/root.store';
+import { RootStoreContext } from "../stores/root.store";
 
 const PrivateRoute = observer((props) => {
-    const rootStore = useContext(RootStoreContext);
-    const isLoggedIn = rootStore.walletStore.hasAccount;
-    const {component: Component, location, ...rest} = props;
+  const rootStore = useContext(RootStoreContext);
+  const isLoggedIn = rootStore.walletStore.hasAccount;
+  const { component: Component, location, ...rest } = props;
 
-    useEffect(
-        () => {
-            if (!isLoggedIn && location.pathname !== `/app/login`) {
-                // If the user is not logged in, redirect to the login page.
-                console.log("PrivateRoute redirect");
-                navigate(`/app/login`);
-            }
-        },
-        [isLoggedIn, location]
-    );
-    return isLoggedIn ? <Component {...rest} /> : null;
+  useEffect(() => {
+    if (!isLoggedIn && location.pathname !== `/app/login`) {
+      // If the user is not logged in, redirect to the login page.
+      navigate(`/app/login`);
+    }
+  }, [isLoggedIn, location]);
+  return isLoggedIn ? <Component {...rest} /> : null;
 });
 
 export default PrivateRoute;
