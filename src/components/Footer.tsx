c9becd041fa9571093586ef75934860963e69707
Chase Manning <me@chasemanning.co.nz>
2021-03-28 11:25:59 +1300

    :rocket: add routing links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Container = styled.footer`
   position: relative;
@@ -15,8 +16,17 @@ const Section = styled.div`
   font-weight: 500;
 `;
 
-const Link = styled.a`
-  margin: 0 10px;
+const InternalLink = styled(Link)`
+  margin-right: 20px;
+  text-decoration: none;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const ExternalLink = styled.a`
+  margin-left: 20px;
   text-decoration: none;
 
   :hover {
@@ -35,37 +45,41 @@ const Footer = () => {
   return (
     <Container>
       <Section>
-        <Link href="/provenance">Provenance</Link>
-        <Link href="/history">History</Link>
+        <InternalLink to={"/provenance"}>Provenance</InternalLink>
+        <InternalLink to={"/history"}>History</InternalLink>
       </Section>
       <AbsoluteCenterSection>
         Made with ❤️ by the Waifusion comunity
       </AbsoluteCenterSection>
       <Section>
-        <Link
+        <ExternalLink
           href="https://twitter.com/waifusion"
           target="_blank"
           rel="noreferrer"
         >
           Twitter
-        </Link>
-        <Link
+        </ExternalLink>
+        <ExternalLink
           href="https://discord.com/invite/q5hRZR72wm"
           target="_blank"
           rel="noreferrer"
         >
           Discord
-        </Link>
-        <Link href="https://t.me/Waifusion" target="_blank" rel="noreferrer">
+        </ExternalLink>
+        <ExternalLink
+          href="https://t.me/Waifusion"
+          target="_blank"
+          rel="noreferrer"
+        >
           Telegram
-        </Link>
-        <Link
+        </ExternalLink>
+        <ExternalLink
           href="mailto: waifusiongovernance@gmail.com"
           target="_blank"
           rel="noreferrer"
         >
           Contact
-        </Link>
+        </ExternalLink>
       </Section>
     </Container>
   );
