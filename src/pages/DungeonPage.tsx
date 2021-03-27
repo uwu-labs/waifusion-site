ce476f91560dd18e87917ec6e1ea0ac0f853a8ae
Chase Manning <me@chasemanning.co.nz>
2021-03-28 11:19:35 +1300

    :sparkles: create dungeon page component

---
+++ src/pages/DungeonPage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledDungeonPage = styled.div``;
+
+const DungeonPage = () => {
+  return <StyledDungeonPage>Dungeon Page</StyledDungeonPage>;
+};
+
+export default DungeonPage;
