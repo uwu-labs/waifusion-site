015d2656ef111f019d86a166408466e7a81201fd
Chase Manning <me@chasemanning.co.nz>
2021-03-28 08:41:37 +1300

    :rocket: add content to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,60 @@ const Container = styled.footer`
   width: 100%;
   padding: 2rem;
   box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Section = styled.div`
+  display: flex;
+  font-weight: 500;
+`;
+
+const Link = styled.a`
+  margin: 0 10px;
+  text-decoration: none;
+
+  :hover {
+    text-decoration: underline;
+  }
 `;
 
 const Footer = () => {
   return (
     <Container>
-      footer items will go here :)
+      <Section>
+        <Link href="/provenance">Provenance</Link>
+        <Link href="/history">History</Link>
+      </Section>
+      <Section>Made with ❤️ by the Waifusion comunity</Section>
+      <Section>
+        <Link
+          href="https://twitter.com/waifusion"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Twitter
+        </Link>
+        <Link
+          href="https://discord.com/invite/q5hRZR72wm"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Discord
+        </Link>
+        <Link href="https://t.me/Waifusion" target="_blank" rel="noreferrer">
+          Telegram
+        </Link>
+        <Link
+          href="mailto: waifusiongovernance@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Contact
+        </Link>
+      </Section>
     </Container>
-  )
-}
+  );
+};
 
 export default Footer;
