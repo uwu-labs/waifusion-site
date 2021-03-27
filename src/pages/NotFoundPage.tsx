051f726744e149804252029260c75b45bf43143e
Chase Manning <me@chasemanning.co.nz>
2021-03-28 11:29:52 +1300

    :sparkles: create 404 page component

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledNotFoundPage = styled.div``;
+
+const NotFoundPage = () => {
+  return <StyledNotFoundPage>404 Page</StyledNotFoundPage>;
+};
+
+export default NotFoundPage;
