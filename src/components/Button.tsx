28d1cab8328e838e340c6af46d3ff5fb138f57ca
Chase Manning <me@chasemanning.co.nz>
2021-03-28 09:10:35 +1300

    :lipstick: added button microinteractions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,31 @@
 import styled from "styled-components";
 
-export const Button = styled.button<{danger?: boolean}>`
+export const Button = styled.button<{ danger?: boolean }>`
   min-width: 10rem;
-  color: ${({danger}) => danger ? "#ffffff" : "#817d82"};
-  border-radius: .5rem;
+  color: ${({ danger }) => (danger ? "#ffffff" : "#817d82")};
+  border-radius: 0.5rem;
   outline: none;
   display: inline-flex;
-  border: 2px solid ${({danger}) => danger ? "#c23d3d" : "#e7e4e7"};
+  border: 2px solid ${({ danger }) => (danger ? "#c23d3d" : "#e7e4e7")};
   font-family: "Calibre";
   height: 3rem;
-  background-color: ${props => props.danger ? "#ef5252" : "transparent"};
+  background-color: ${(props) => (props.danger ? "#ef5252" : "transparent")};
   align-items: center;
   justify-content: space-around;
-  box-shadow: 0 .2rem 0 0 ${({danger}) => danger ? "#c23d3d" : "#e7e4e7"};;
+  box-shadow: 0 0.2rem 0 0 ${({ danger }) => (danger ? "#c23d3d" : "#e7e4e7")};
   font-weight: 500;
   line-height: 1;
   font-size: 14pt;
+  transition: all 0.3s;
+  cursor: pointer;
+
+  :hover {
+    box-shadow: 0 0.3rem 0 0 ${({ danger }) => (danger ? "#c23d3d" : "#e7e4e7")};
+    transform: translateY(-0.1rem);
+  }
+
+  :active {
+    box-shadow: 0 0 0 0 ${({ danger }) => (danger ? "#c23d3d" : "#e7e4e7")};
+    transform: translateY(0.2rem);
+  }
 `;
