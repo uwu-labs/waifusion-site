3a2c34e1e59416ebd6d1eeeb7112b137743e1b1f
Chase Manning <me@chasemanning.co.nz>
2021-03-28 07:34:13 +1300

    :art: move body and code css to global styles

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,19 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  body {
+    margin: 0;
+    font-family: 'Calibre', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
 `;
 
 function App() {
