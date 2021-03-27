de86b0f4ac15f0040412a0039f900d510a5ab7a7
Chase Manning <me@chasemanning.co.nz>
2021-03-28 11:28:24 +1300

    :sparkles: create home page component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledHomePage = styled.div``;
+
+const HomePage = () => {
+  return <StyledHomePage>Home Page</StyledHomePage>;
+};
+
+export default HomePage;
