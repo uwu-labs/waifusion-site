1e7e32c34836fafbe9feae7242e36d7738712387
Chase Manning <me@chasemanning.co.nz>
2021-03-28 11:22:08 +1300

    :sparkles: create history page component

---
+++ src/pages/HistoryPage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledHistoryPage = styled.div``;
+
+const HistoryPage = () => {
+  return <StyledHistoryPage>History Page</StyledHistoryPage>;
+};
+
+export default HistoryPage;
