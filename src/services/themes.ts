96219aac6cffae65f783f7f0291ccda364edd1d7
Chase Manning <me@chasemanning.co.nz>
2021-03-28 09:23:52 +1300

    :sparkles: add shadow colors to theme

--- src/services/themes.ts
+++ src/services/themes.ts
@@ -7,8 +7,11 @@ export const defaultTheme = {
   textSecondary: "#817d82",
 
   primary: "#F9C5F7",
+  primaryShadow: "#DE9CDC",
   secondary: "#FFEFC2",
+  secondaryShadow: "#EBD493",
   highlight: "#16899B",
+  highlightShadow: "#1F4A51",
   danger: "#ef5252",
   dangerShadow: "#c23d3d",
 };
