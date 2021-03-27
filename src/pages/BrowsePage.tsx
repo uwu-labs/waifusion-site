744b8ac53bc14b5720f9cb322554c0fc1f866286
Chase Manning <me@chasemanning.co.nz>
2021-03-28 11:17:16 +1300

    :sparkles: create browse page component

---
+++ src/pages/BrowsePage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledBrowsePage = styled.div``;
+
+const BrowsePage = () => {
+  return <StyledBrowsePage>Browse Page</StyledBrowsePage>;
+};
+
+export default BrowsePage;
