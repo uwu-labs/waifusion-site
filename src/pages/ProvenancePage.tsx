cb5a94b129f7cfac514e59034067bc6fd12e2bfe
Chase Manning <me@chasemanning.co.nz>
2021-03-28 11:21:08 +1300

    :sparkles: create provenance page component

---
+++ src/pages/ProvenancePage.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledProvenancePage = styled.div``;
+
+const ProvenancePage = () => {
+  return <StyledProvenancePage>Provenance Page</StyledProvenancePage>;
+};
+
+export default ProvenancePage;
