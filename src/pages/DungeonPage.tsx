f38d7329f331a5c9e61043a122ffab1b0fdd2973
Chase Manning <me@chasemanning.co.nz>
2021-03-28 16:13:45 +1300

    :rocket: add header to dungeon page

--- src/pages/DungeonPage.tsx
+++ src/pages/DungeonPage.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import Header from "../components/Header";
 
 const StyledDungeonPage = styled.div``;
 
 const DungeonPage = () => {
-  return <StyledDungeonPage>Dungeon Page</StyledDungeonPage>;
+  return (
+    <StyledDungeonPage>
+      <Header text={"Waifu Dungeon"} />
+    </StyledDungeonPage>
+  );
 };
 
 export default DungeonPage;
