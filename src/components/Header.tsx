303fd88691730c8d47153fcb85ba0bc107000f4a
Chase Manning <me@chasemanning.co.nz>
2021-03-28 18:33:53 +1300

    :art: change header to use underline component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import styled, { keyframes } from "styled-components";
+import Underline from "./Underline";
 
 const StyledHeader = styled.div`
   display: flex;
@@ -9,33 +10,6 @@ const Text = styled.h1`
   font-size: 5rem;
   font-weight: 600;
   color: var(--text-primary);
-`;
-
-const rotate = keyframes`
-    0% {
-        background-position: 0% 50%;
-    }
-    100% {
-        background-position: 150% 50%;
-    }
-`;
-
-const Underline = styled.div`
-  height: 3px;
-  width: 100%;
-  filter: saturate(1.5);
-  background: linear-gradient(
-      45deg,
-      var(--primary) 0%,
-      var(--secondary) 25%,
-      var(--highlight) 50%,
-      var(--secondary) 75%,
-      var(--primary) 100%
-    )
-    0% 0% / 300% 300%;
-  transform: translateY(-1rem);
-  animation: ${rotate} 3s linear 0s infinite;
-  border-radius: 1px;
 `;
 
 type Props = {
