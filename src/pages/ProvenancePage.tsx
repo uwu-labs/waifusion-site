ace390ab5fa2e1c3ea68aeeae89c6b57f0643ac6
Chase Manning <me@chasemanning.co.nz>
2021-03-28 16:15:39 +1300

    :rocket: add header to provenance page

--- src/pages/ProvenancePage.tsx
+++ src/pages/ProvenancePage.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import Header from "../components/Header";
 
 const StyledProvenancePage = styled.div``;
 
 const ProvenancePage = () => {
-  return <StyledProvenancePage>Provenance Page</StyledProvenancePage>;
+  return (
+    <StyledProvenancePage>
+      <Header text={"Provenance"} />
+    </StyledProvenancePage>
+  );
 };
 
 export default ProvenancePage;
