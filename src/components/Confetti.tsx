c79cd8872c5d4fa0ad0ef183e0b450f38af48d0b
Chase Manning <me@chasemanning.co.nz>
2021-03-28 22:12:06 +1300

    :lipstick: change confetti to cherry blossom image

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
+import sakura from "../assets/sakura.png";
 
 const CONFETTI_COUNT = 40;
 
@@ -18,7 +19,24 @@ const StyledConfetti = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+  overflow: hidden;
 `;
+
+// const float = keyframes`
+//     0% {
+//         transform: translate(-20px, -5px);
+//     }
+//     50% {
+//         transform: translate(0, 0);
+//     }
+//     100% {
+//         transform: translate(20px, -2px);
+//     }
+// `;
+
+// const FloatAnimation = styled.div`
+//   animation: ${float} 3s alternate 0s infinite;
+// `;
 
 const rotate = keyframes`
     0% {
@@ -29,15 +47,10 @@ const rotate = keyframes`
     }
 `;
 
-const Animate = styled.div`
-  animation: ${rotate} 3s linear 0s infinite;
-`;
-
-const ConfettiItem = styled.div`
+const ConfettiItem = styled.img`
   position: absolute;
   border-radius: 2px;
   animation: ${rotate} 20s linear 0s infinite;
-  background-color: var(--primary);
 `;
 
 const Confetti = () => {
@@ -46,14 +59,14 @@ const Confetti = () => {
   useEffect(() => {
     let _confetti: ConfettiType[] = [];
     for (let i = 0; i < CONFETTI_COUNT; i++) {
-      const width = Math.random() * 50 + 10;
+      const width = Math.random() * 30 + 30;
 
       _confetti.push({
         left: Math.random() * 200 - 100,
         top: Math.random() * 200 - 100,
         width: width,
         height: width / 2,
-        blur: (width - 10) / 5,
+        blur: (width - 10) / 10,
         rotation: Math.random() * 360,
       });
       console.log("meow");
@@ -65,6 +78,7 @@ const Confetti = () => {
     <StyledConfetti>
       {confetti.map((conf: ConfettiType) => (
         <ConfettiItem
+          src={sakura}
           style={{
             left: conf.left + "%",
             top: conf.top + "%",
