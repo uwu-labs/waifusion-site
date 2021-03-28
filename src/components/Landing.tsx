4f8bb37cdd8ed17189df47949e17908fff3c29bc
Chase Manning <me@chasemanning.co.nz>
2021-03-28 21:02:59 +1300

    :rocket: add rainbow effect to landing card

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Header from "./Header";
 import landing from "../assets/landing.png";
 import Button from "./Button";
@@ -21,20 +21,43 @@ const Image = styled.img`
   width: 50vw;
 `;
 
+const rotate = keyframes`
+    0% {
+        background-position: 0% 50%;
+    }
+    100% {
+        background-position: 150% 50%;
+    }
+`;
+
 const Background = styled.div`
+  box-shadow: 0px 4px 15px 15px rgba(0, 0, 0, 0.04);
   width: 60vw;
-  padding: 1px;
+  padding: 2px 2px 6px 2px;
+  border-radius: 1.1rem;
+  background-color: red;
+  transform: translateY(-0.5rem);
+  filter: saturate(1.5);
+  background: linear-gradient(
+      45deg,
+      var(--primary) 0%,
+      var(--secondary) 25%,
+      var(--highlight) 50%,
+      var(--secondary) 75%,
+      var(--primary) 100%
+    )
+    0% 0% / 300% 300%;
+  animation: ${rotate} 3s linear 0s infinite;
 `;
 
 const Card = styled.div`
   width: 100%;
   height: 100%;
   background-color: var(--plain);
-  box-shadow: 0 0.2rem 0 0 var(--plain-shadow);
-  border: 2px solid var(--plain-shadow);
-  padding: 1rem;
+  /* box-shadow: 0 0.2rem 0 0 var(--plain-shadow);
+  border: 2px solid var(--plain-shadow); */
+  padding: 1.8rem;
   border-radius: 1rem;
-  transform: translateY(-0.5rem);
   display: flex;
   flex-direction: column;
   align-items: center;
