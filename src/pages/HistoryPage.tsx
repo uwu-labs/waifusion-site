9dc1014972c4570c48bcc341736c662123304052
Chase Manning <me@chasemanning.co.nz>
2021-03-28 16:15:27 +1300

    :rocket: add header to history page

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import Header from "../components/Header";
 
 const StyledHistoryPage = styled.div``;
 
 const HistoryPage = () => {
-  return <StyledHistoryPage>History Page</StyledHistoryPage>;
+  return (
+    <StyledHistoryPage>
+      <Header text={"Your History"} />
+    </StyledHistoryPage>
+  );
 };
 
 export default HistoryPage;
