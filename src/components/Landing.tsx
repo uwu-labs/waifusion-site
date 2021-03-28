d05025bfb8e41cd853de33319f2916f8fee21885
Chase Manning <me@chasemanning.co.nz>
2021-03-28 20:55:32 +1300

    :rocket: add card to landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Header from "./Header";
 import landing from "../assets/landing.png";
+import Button from "./Button";
 
 const StyledLanding = styled.div`
   width: 100%;
@@ -10,14 +11,43 @@ const StyledLanding = styled.div`
 `;
 
 const SubHeader = styled.div`
-  font-size: 1.8rem;
+  font-size: 1.7rem;
   font-weight: 500;
   color: var(--text-secondary);
-  margin-bottom: 4rem;
+  margin-bottom: 3rem;
 `;
 
 const Image = styled.img`
   width: 50vw;
+`;
+
+const Background = styled.div`
+  width: 60vw;
+  padding: 1px;
+`;
+
+const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: var(--plain);
+  box-shadow: 0 0.2rem 0 0 var(--plain-shadow);
+  border: 2px solid var(--plain-shadow);
+  padding: 1rem;
+  border-radius: 1rem;
+  transform: translateY(-0.5rem);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CardText = styled.p`
+  font-size: 1.2rem;
+  font-weight: 500;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 2rem;
+  display: flex;
 `;
 
 const Landing = () => {
@@ -26,6 +56,19 @@ const Landing = () => {
       <Header text={"Waifusion"} />
       <SubHeader>No Waifu, No Laifu</SubHeader>
       <Image src={landing} />
+      <Background>
+        <Card>
+          <CardText>
+            Waifusion is a digital Waifu collection. There are 16,384
+            guaranteed-unique Waifusion NFTs. They’re just like you; a beautiful
+            work of art, but 2-D and therefore, superior, Anon-kun. Each Waifu
+            is wholly unique and yours forever... unless you sell them... Baka.
+          </CardText>
+          <ButtonContainer>
+            <Button primary>Get Waifus</Button>
+          </ButtonContainer>
+        </Card>
+      </Background>
     </StyledLanding>
   );
 };
