92713e4c16f3547434f79d00e55395e2c84a2b71
Chase Manning <me@chasemanning.co.nz>
2021-03-28 16:15:43 +1300

    :rocket: add header to wallet page

--- src/pages/WalletPage.tsx
+++ src/pages/WalletPage.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import Header from "../components/Header";
 
 const StyledWalletPage = styled.div``;
 
 const WalletPage = () => {
-  return <StyledWalletPage>Wallet Page</StyledWalletPage>;
+  return (
+    <StyledWalletPage>
+      <Header text={"Your Wallet"} />
+    </StyledWalletPage>
+  );
 };
 
 export default WalletPage;
