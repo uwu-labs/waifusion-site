e998217fb0b56703fddbe1fe66faede98acf3f62
Phineas <phin@phineas.io>
2021-03-29 02:53:59 +0100

    fix total accumulated counter; styling fixes

--- src/app/private-pages/wallet.js
+++ src/app/private-pages/wallet.js
@@ -37,7 +37,7 @@ const Main = observer((props) => {
       await updateWETState();
       WETStore.setIsLoading(false);
     }
-    walletStore.loginWalletIfNeeded()
+    walletStore.loginWalletIfNeeded();
     updateWETS();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -101,17 +101,22 @@ const Main = observer((props) => {
     let totalAccumulated = new BN(0);
     WETStore.totalAccumulatedLoading = true;
 
-
     const t = await accoomulate();
 
-    t.forEach(async(token) => {
+    await t.forEach(async (token) => {
       const accumulated = new BN(token.wetAccumulated);
-      const accumulatedWETNumber = Number(await toEthUnit(accumulated)).toFixed(2);
+      const accumulatedWETNumber = Number(await toEthUnit(accumulated)).toFixed(
+        2
+      );
 
-      WETStore.addOwnedItem({ index: token.tokenId, name: token.name, id: token.tokenId, accumulatedWETNumber});
+      WETStore.addOwnedItem({
+        index: token.tokenId,
+        name: token.name,
+        id: token.tokenId,
+        accumulatedWETNumber,
+      });
       totalAccumulated = totalAccumulated.add(accumulated);
-      console.log(accumulated)
-    })
+    });
 
     WETStore.totalAccumulated = Number(
       await toEthUnit(totalAccumulated)
@@ -139,7 +144,12 @@ const Main = observer((props) => {
               <Flex className="wallet-qr-container">
                 <Box>
                   <div>
-                    <QR value={walletStore.defaultAddress} size={308} bgColor={"#181425"} fgColor={"#ffffff"} />
+                    <QR
+                      value={walletStore.defaultAddress}
+                      size={308}
+                      bgColor={"#181425"}
+                      fgColor={"#ffffff"}
+                    />
                   </div>
                 </Box>
                 <Box col-sm-4>
@@ -212,10 +222,26 @@ const Main = observer((props) => {
                   <br />
                   <span className="card-box-center-pink-text test-margin">
                     <span className="waifu-box-number">Waifu ID: </span>
-                    <span className="waifu-box-number">{item.id}
-                    <svg style={{height:'2vh', marginLeft:'1vw'}} aria-hidden="true" focusable="false" data-prefix="far" data-icon="copy" role="img" viewBox="0 0 448 512" className="svg-inline--fa fa-copy fa-w-14 fa-2x"
-                    onClick={() => {navigator.clipboard.writeText(item.id)}}>
-                      <path fill="currentColor" d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"></path></svg>
+                    <span className="waifu-box-number">
+                      {item.id}
+                      <svg
+                        style={{ height: "2vh", marginLeft: "1vw" }}
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="far"
+                        data-icon="copy"
+                        role="img"
+                        viewBox="0 0 448 512"
+                        className="svg-inline--fa fa-copy fa-w-14 fa-2x"
+                        onClick={() => {
+                          navigator.clipboard.writeText(item.id);
+                        }}
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"
+                        ></path>
+                      </svg>
                     </span>
                   </span>
                   <img
@@ -264,12 +290,14 @@ const Main = observer((props) => {
                       {item.index} / {GLOBALS.TOTAL_WAIFUS}
                     </span>
                   </span>
-                  
+
                   <br />
                   <br />
                   <span className="card-box-center-pink-text test-margin">
                     <span className="waifu-box-number">Unclaimed WET: </span>
-                    <span className="waifu-box-number">{item.accumulatedWETNumber}</span>
+                    <span className="waifu-box-number">
+                      {item.accumulatedWETNumber}
+                    </span>
                   </span>
                 </center>
               </Box>
