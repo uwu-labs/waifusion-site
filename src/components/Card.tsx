ceffe44cd401bdbb6717e22a1ac7615c5e906292
Chase Manning <me@chasemanning.co.nz>
2021-03-29 16:52:12 +1300

    :lipstick: update card size and color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -41,8 +41,9 @@ const StyledCard = styled.div`
 `;
 
 const CardText = styled.p`
-  font-size: 1.2rem;
+  font-size: 1.4rem;
   font-weight: 500;
+  color: var(--plain-dark);
   text-align: center;
 `;
 
