499f9afc8f7cf4ae06d0051dcfe8c02b8a8375f9
Chase Manning <me@chasemanning.co.nz>
2021-03-29 16:52:43 +1300

    :lipstick: add rainbow background to slide

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,13 +1,33 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Button from "./Button";
+
+const rotate = keyframes`
+    0% {
+        background-position: 0% 50%;
+    }
+    100% {
+        background-position: 150% 50%;
+    }
+`;
 
 const StyledSlide = styled.div`
   width: 100%;
   display: flex;
-  height: 50vh;
+  padding: 5rem 0;
   justify-content: center;
   align-items: center;
   background-color: var(--primary);
+  filter: saturate(1.5);
+  background: linear-gradient(
+      90deg,
+      var(--primary) 0%,
+      var(--secondary) 25%,
+      var(--highlight) 50%,
+      var(--secondary) 75%,
+      var(--primary) 100%
+    )
+    0% 0% / 300% 300%;
+  animation: ${rotate} 10s linear 0s infinite;
 `;
 
 const Card = styled.div`
@@ -36,6 +56,7 @@ const Body = styled.p`
   font-weight: 500;
   color: var(--plain-dark);
   margin-bottom: 2rem;
+  text-align: center;
 `;
 
 const Slide = () => {
@@ -44,12 +65,17 @@ const Slide = () => {
       <Card>
         <Header>WET Token</Header>
         <Body>
-          COntent contentCOntent content COntent content COntent content COntent
-          content COntent content COntent contentCOntent content COntent content
-          COntent content COntent COntent contentCOntent content COntent content
-          COntent content COntent COntent contentCOntent content COntent content
-          COntent content COntent content COntent content content COntent
-          content content COntent content
+          The Waifu Enhancement Token (WET) is an exclusive token to the
+          Waifusion world wide harem. WET serves two purposes: Give Waifus a
+          unique name that is permanently stored and publicly visible on the
+          Ethereum Blockchain. Burning an undesirable Waifu in exchange for a
+          new Waifu from the Dungeon If you own a Waifu, you can claim the WETs
+          that are constantly being produced by them being in your harem. Each
+          Waifu will drip around 3,660 WETs per year. To change the name of any
+          Waifu, you need to send 1,830 WETs (about ½ years of Waifu emission)
+          to the Waifusion contract with the new name. To burn a Waifu you need
+          to send a Waifu and 5,490 WETs to the Waifusion contract they will
+          disappear forever...
         </Body>
         <Button secondary>Get WET</Button>
       </Card>
