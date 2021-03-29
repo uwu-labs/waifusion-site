e1681d53eb50853c1ce320937668319012ae237c
Chase Manning <me@chasemanning.co.nz>
2021-03-29 17:26:45 +1300

    :sparkles: create preview component

---
+++ src/components/Preview.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const StyledPreview = styled.div`
+  width: 100%;
+  padding: 3rem;
+  min-height: 60vh;
+`;
+
+const Preview = () => {
+  return (
+    <StyledPreview>
+      This could be an example of some waifus maybe? Recently sold ones or
+      something.
+    </StyledPreview>
+  );
+};
+
+export default Preview;
