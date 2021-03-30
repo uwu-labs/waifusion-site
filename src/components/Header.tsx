b1758f65b59f50913b866dbefda36fa66bfe8f36
Chase Manning <me@chasemanning.co.nz>
2021-03-30 22:38:34 +1300

    :lipstick: left align header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const StyledHeader = styled.div`
   display: flex;
   flex-direction: column;
-  text-align: center;
+  text-align: left;
 `;
 
 const Text = styled.h1`
