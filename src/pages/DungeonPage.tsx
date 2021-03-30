3c9aa2b2aba97a9645e3463eedd53672d782871a
Chase Manning <me@chasemanning.co.nz>
2021-03-30 22:20:05 +1300

    :rocket: add card to dungeon page

--- src/pages/DungeonPage.tsx
+++ src/pages/DungeonPage.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
+import Card from "../components/Card";
+import { PageContentWrapper } from "../components/CommonLayout";
 import Header from "../components/Header";
 
-const StyledDungeonPage = styled.div``;
+const StyledDungeonPage = styled(PageContentWrapper)`
+  height: 70vh;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const CardContainer = styled.div`
+  width: 50vw;
+`;
 
 const DungeonPage: React.FC = () => {
   const [t] = useTranslation();
@@ -11,6 +27,16 @@ const DungeonPage: React.FC = () => {
   return (
     <StyledDungeonPage>
       <Header text={t("headers.dungeon")} />
+      <Content>
+        <CardContainer>
+          <Card
+            text="Oh no! Many of your beloved waifus are enslaved in the Dungeon! Be a hero and save your beloved by buying her freedom, or be the villain and burn one of your current Waifus with WET in exchange for a new one from the dungeon."
+            buttonAction={() => alert("Not implemented yet")}
+            buttonText="Rescue Waifu"
+          />
+        </CardContainer>
+        <div>me</div>
+      </Content>
     </StyledDungeonPage>
   );
 };
