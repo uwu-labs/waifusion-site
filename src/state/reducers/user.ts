53858bb650bc8adbe1c6aab8c803e2ae94e0871f
Chase Manning <me@chasemanning.co.nz>
2021-04-02 19:11:16 +1300

    :sparkles: add total accumulated selector

--- src/state/reducers/user.ts
+++ src/state/reducers/user.ts
@@ -33,5 +33,10 @@ export const selectUsersWaifus = (state: RootState) => {
     (waifu: Waifu) => state.user.waifuIndexes.indexOf(waifu.id) > -1
   );
 };
+export const selectTotalAccumulated = (state: RootState) => {
+  return state.waifus.waifus
+    .filter((waifu: Waifu) => state.user.waifuIndexes.indexOf(waifu.id) > -1)
+    .reduce((a: number, b: Waifu) => a + (b.accumulatedWet || 0), 0);
+};
 
 export default userSlice.reducer;
