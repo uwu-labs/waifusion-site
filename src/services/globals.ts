65ad4fc367005871aea8c292e07050c80b213d67
Chase Manning <me@chasemanning.co.nz>
2021-04-02 15:03:16 +1300

    :sparkles: create globals

---
+++ src/services/globals.ts
@@ -0,0 +1,56 @@
+type Globals = {
+  WAIFU_VERSION: string;
+  WALLET_TYPE_WALLETCONNECT: string;
+  WALLET_TYPE_METAMASK: string;
+  WAIFU_CONTRACT_ADDRESS: string;
+  WET_CONTRACT_ADDRESS: string;
+  ACCOOMULATOR_CONTRACT_ADDRESS: string;
+  DUNGEON_CONTRACT_ADDRESS: string;
+  INFURA_API: string;
+  STARTING_INDEX: number;
+  TOTAL_WAIFUS: number;
+  NAME_CHANGE_PRICE: number;
+  APPROVE_AMOUNT: string;
+  BUY_PRICE: string;
+  CURRENCY: string;
+};
+
+const waifuVersion =
+  typeof window !== "undefined" &&
+  window.location.hostname === "waifusionbsc.sexy"
+    ? "bsc"
+    : "eth";
+
+const GLOBALS: Globals = {
+  WAIFU_VERSION: waifuVersion,
+  WALLET_TYPE_WALLETCONNECT: "walletconnect",
+  WALLET_TYPE_METAMASK: "metamask",
+  WAIFU_CONTRACT_ADDRESS: "0x2216d47494E516d8206B70FCa8585820eD3C4946",
+  WET_CONTRACT_ADDRESS: "0x76280AF9D18a868a0aF3dcA95b57DDE816c1aaf2",
+  ACCOOMULATOR_CONTRACT_ADDRESS:
+    waifuVersion === "eth"
+      ? "0x5cEB3ec62f8151628a6A645da52F0ba7e6d6de26"
+      : "0xe21580d17708b29455a42602B3A964B9a1BFBCCa",
+  DUNGEON_CONTRACT_ADDRESS:
+    waifuVersion === "eth"
+      ? "0xB291984262259BcFe6Aa02b66a06e9769C5c1eF3"
+      : "0x21d2cf043937dcbaeff1feff75776f526b0c83d9",
+  INFURA_API:
+    waifuVersion === "bsc"
+      ? "https://bsc-dataseed.binance.org/"
+      : `https://mainnet.infura.io/v3/b732460c3da849bca6067e7bb72f4bee`,
+  STARTING_INDEX: waifuVersion === "eth" ? 11595 : 11936,
+  TOTAL_WAIFUS: 16384,
+  NAME_CHANGE_PRICE: 1830000000000000000000,
+  APPROVE_AMOUNT: "16470000000000000000000",
+  BUY_PRICE: waifuVersion === "eth" ? "0.7" : "1.8",
+  CURRENCY: waifuVersion === "eth" ? "ETH" : "BNB",
+};
+
+export default GLOBALS;
+
+/*Test contracts */
+// GLOBALS.WAIFU_CONTRACT_ADDRESS = "0xe6a4b81DB7d82d91820f7BB4f63855cBb25139bc";
+// GLOBALS.WET_CONTRACT_ADDRESS = "0x24884ED967c1B9A33e384f67E185e81afEEFaD55";
+// GLOBALS.DUNGEON_CONTRACT_ADDRESS = "0x18f921e62E7196Fddf5CDE1e8df53fE16FfECC19";
+// GLOBALS.INFURA_API = `https://ropsten.infura.io/v3/b732460c3da849bca6067e7bb72f4bee`;
