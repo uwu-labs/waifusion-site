0237ac0e218ad8e0d524455d02ce2e649c3823f6
Chase Manning <me@chasemanning.co.nz>
2021-04-02 16:13:59 +1300

    :rocket: add wallet loading to wallet page

--- src/pages/WalletPage.tsx
+++ src/pages/WalletPage.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import Header from "../components/Header";
+import { getWaifus } from "../services/contract";
+import { Waifu } from "../types/waifusion";
 
 const StyledWalletPage = styled.div``;
 
 const WalletPage: React.FC = () => {
   const [t] = useTranslation();
+  const [ownedWaifus, setOwnedWaifus] = useState<Waifu[]>([]);
+
+  const setWallet = async () => {
+    const _ownedWaifus = await getWaifus();
+    setOwnedWaifus(_ownedWaifus);
+  };
+
+  useEffect(() => {
+    setWallet();
+  }, []);
 
   return (
     <StyledWalletPage>
       <Header text={t("headers.wallet")} />
+      {ownedWaifus.map((waifu: Waifu) => (
+        <p>{waifu.id}</p>
+      ))}
     </StyledWalletPage>
   );
 };
