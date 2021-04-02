225158dee79878973392cae8383d8bd29a7f28cc
Chase Manning <me@chasemanning.co.nz>
2021-04-02 17:34:18 +1300

    :sparkles: create waifus reducer

---
+++ src/state/reducers/waifus.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Waifu } from "../../types/waifusion";
+import { RootState } from "../index";
+
+interface WaifusState {
+  waifus: Waifu[];
+}
+
+const initialState: WaifusState = {
+  waifus: [],
+};
+
+export const waifusSlice = createSlice({
+  name: "waifus",
+  initialState,
+  reducers: {
+    addWaifu: (state, action: PayloadAction<Waifu>) => {
+      state.waifus.push(action.payload);
+    },
+  },
+});
+
+export const { addWaifu } = waifusSlice.actions;
+
+export const selectWaifus = (state: RootState) => state.waifus.waifus;
+
+export default waifusSlice.reducer;
