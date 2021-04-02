43bcb09b9347cbba1e161d1a4d48cf768f9abc56
Chase Manning <me@chasemanning.co.nz>
2021-04-02 17:34:57 +1300

    :art: change users waifus to map to waifus reducer

--- src/pages/WalletPage.tsx
+++ src/pages/WalletPage.tsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import Header from "../components/Header";
 import { ContractHelper, getAddress } from "../services/contract";
+import { selectUsersWaifus, setWaifuIndexes } from "../state/reducers/user";
+import { addWaifu, selectWaifus } from "../state/reducers/waifus";
 import { Waifu } from "../types/waifusion";
 
 const StyledWalletPage = styled.div``;
 
 const WalletPage: React.FC = () => {
+  const dispatch = useDispatch();
+  const usersWaifus = useSelector(selectUsersWaifus);
+  const waifus = useSelector(selectWaifus);
   const [t] = useTranslation();
-  const [ownedWaifus, setOwnedWaifus] = useState<Waifu[]>([]);
 
   const loadOwnedWaifus = async () => {
     const address = await getAddress();
     const contractHelper = new ContractHelper(address);
     const _ownedWaifus = await contractHelper.getWaifus();
-    setOwnedWaifus(_ownedWaifus);
+    dispatch(setWaifuIndexes(_ownedWaifus.map((waifu: Waifu) => waifu.id)));
+    _ownedWaifus.forEach((ownedWaifu: Waifu) => {
+      if (waifus.map((waifu: Waifu) => waifu.id).indexOf(ownedWaifu.id) === -1)
+        dispatch(addWaifu(ownedWaifu));
+    });
   };
 
   useEffect(() => {
@@ -25,7 +34,7 @@ const WalletPage: React.FC = () => {
   return (
     <StyledWalletPage>
       <Header text={t("headers.wallet")} />
-      {ownedWaifus.map((waifu: Waifu) => (
+      {usersWaifus.map((waifu: Waifu) => (
         <p>{waifu.id}</p>
       ))}
     </StyledWalletPage>
