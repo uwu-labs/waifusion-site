2cf8d9b6f922e9c486ac40f77c7af5df5c432bed
Chase Manning <me@chasemanning.co.nz>
2021-04-05 10:43:40 +1200

    :art: change browse page to use filter component

--- src/pages/BrowsePage.tsx
+++ src/pages/BrowsePage.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { PageContentWrapper } from "../components/CommonLayout";
 import Header from "../components/Header";
 import WaifuCard from "../components/WaifuCard";
+import Filter from "../components/Filter";
 import traits, { Trait } from "../data/traits";
 import { makeRequest } from "../services/api";
 import { Waifu } from "../types/waifusion";
@@ -34,26 +35,6 @@ const Filters = styled.div`
   display: flex;
   flex-direction: column;
   margin-right: 3rem;
-`;
-
-const Filter = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 1.3rem;
-`;
-
-const FilterLabel = styled.div`
-  font-size: 1.2rem;
-  font-weight: 400;
-`;
-
-const Select = styled.select`
-  font-size: 1rem;
-  color: var(--text-secondary);
-`;
-
-const Option = styled.option`
-  font-size: 1rem;
 `;
 
 const BrowsePage: React.FC = () => {
@@ -97,15 +78,7 @@ const BrowsePage: React.FC = () => {
         )}
         <Filters>
           {traits.map((trait: Trait) => (
-            <Filter key={trait.id}>
-              <FilterLabel>{`${trait.id}:`}</FilterLabel>
-              <Select onChange={(event) => console.log(event.target.value)}>
-                <Option value="">All</Option>
-                {trait.values.map((value: string) => (
-                  <Option value={value}>{value}</Option>
-                ))}
-              </Select>
-            </Filter>
+            <Filter trait={trait} />
           ))}
         </Filters>
       </Content>
