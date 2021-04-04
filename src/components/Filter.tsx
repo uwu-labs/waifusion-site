08abd98e30cff46bded16c148fb2bd801861cc5b
Chase Manning <me@chasemanning.co.nz>
2021-04-05 10:52:03 +1200

    :rocket: add filter border with raibow option

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Trait } from "../data/traits";
+
+const rotate = keyframes`
+    0% {
+        background-position: 0% 50%;
+    }
+    100% {
+        background-position: 150% 50%;
+    }
+`;
+
+type BackgroundProps = {
+  rainbow?: boolean;
+};
+
+const Background = styled.div`
+  margin-top: 1.3rem;
+  width: 100%;
+  padding: 2px 2px 6px 2px;
+  border-radius: 1.1rem;
+  transform: translateY(-0.5rem);
+  filter: saturate(1.5);
+  background: linear-gradient(
+      45deg,
+      var(--primary) 0%,
+      var(--secondary) 25%,
+      var(--highlight) 50%,
+      var(--secondary) 75%,
+      var(--primary) 100%
+    )
+    0% 0% / 300% 300%;
+  animation: ${rotate} 3s linear 0s infinite;
+  background: ${(props: BackgroundProps) =>
+    props.rainbow ? "auto" : "var(--plain-shadow)"};
+`;
 
 const StyledFilter = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 1.3rem;
+  background-color: white;
+  border-radius: 1rem;
+  padding: 1rem;
 `;
 
 const FilterLabel = styled.div`
@@ -16,6 +52,8 @@ const FilterLabel = styled.div`
 const Select = styled.select`
   font-size: 1rem;
   color: var(--text-secondary);
+  border: none;
+  outline: none;
 `;
 
 const Option = styled.option`
@@ -28,15 +66,17 @@ type Props = {
 
 const Filter: React.FC<Props> = (props) => {
   return (
-    <StyledFilter key={props.trait.id}>
-      <FilterLabel>{`${props.trait.id}:`}</FilterLabel>
-      <Select onChange={(event) => console.log(event.target.value)}>
-        <Option value="">All</Option>
-        {props.trait.values.map((value: string) => (
-          <Option value={value}>{value}</Option>
-        ))}
-      </Select>
-    </StyledFilter>
+    <Background>
+      <StyledFilter key={props.trait.id}>
+        <FilterLabel>{`${props.trait.id}:`}</FilterLabel>
+        <Select onChange={(event) => console.log(event.target.value)}>
+          <Option value="">All</Option>
+          {props.trait.values.map((value: string) => (
+            <Option value={value}>{value}</Option>
+          ))}
+        </Select>
+      </StyledFilter>
+    </Background>
   );
 };
 
