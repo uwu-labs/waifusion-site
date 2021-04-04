9b91b6daa41cd1c5f81d4586b4595ba19e47e45c
Chase Manning <me@chasemanning.co.nz>
2021-04-04 20:48:26 +1200

    :sparkles: create loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import loading from "../assets/loading.gif";
+
+const StyledLoading = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Circle = styled.div`
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+  transition: all 0.6s;
+`;
+
+const Animation = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -40%);
+  width: 300px;
+  height: 300px;
+`;
+
+const Loading = () => {
+  const [size, setSize] = useState("0");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSize("200px");
+    }, 500);
+  }, []);
+
+  return (
+    <StyledLoading>
+      <Circle style={{ width: size, height: size }}>
+        <Animation src={loading} />
+      </Circle>
+    </StyledLoading>
+  );
+};
+
+export default Loading;
