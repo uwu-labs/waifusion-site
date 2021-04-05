58a53e8dc34eed7d0f2e462b36798c7fe07ad22a
Chase Manning <me@chasemanning.co.nz>
2021-04-05 16:26:13 +1200

    :sparkles: create reveal complete popup

---
+++ src/components/RevealComplete.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import Popup from "./Popup";
+
+type Props = {
+  show: boolean;
+  close: () => void;
+};
+
+const RevealComplete: React.FC<Props> = (props) => {
+  return (
+    <Popup
+      show={props.show}
+      close={() => props.close}
+      header="Waifus Revealed!"
+      buttonAction={() => props.close()}
+      buttonText="Close"
+    />
+  );
+};
+
+export default RevealComplete;
