c2907885aeaf9a10c505efd0f407d1d31ce1a196
Chase Manning <me@chasemanning.co.nz>
2021-04-05 15:11:14 +1200

    :rocket: add validation

--- src/components/BuyWaifu.tsx
+++ src/components/BuyWaifu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Input from "./Input";
 import Popup from "./Popup";
@@ -10,24 +10,62 @@ const Content = styled.div`
   align-items: center;
 `;
 
+const Error = styled.div`
+  font-size: 1.2rem;
+  margin-top: 0.5rem;
+  color: var(--danger);
+`;
+
 type Props = {
   show: boolean;
   close: () => void;
 };
 
 const BuyWaifu: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<string>("");
+  const [error, setError] = useState("");
+
+  const validate = () => {
+    setError("");
+    let amount = 0;
+    try {
+      amount = Number(count);
+    } catch {
+      setError("Not a valid number");
+      return;
+    }
+    if (!Number.isInteger(amount)) {
+      setError("Must be a whole number");
+    }
+    if (amount > 20) {
+      setError("Maximum of 20 allowed");
+    }
+    if (amount <= 0) {
+      setError("Number of Waifus cannot be 0 or negative");
+    }
+  };
+
   return (
     <Popup
       show={props.show}
       close={() => props.close()}
       content={
         <Content>
-          <Input type="number" placeholder="1" />
+          <Input
+            value={count}
+            type="number"
+            placeholder="1"
+            onChange={(event) => setCount(event.target.value)}
+          />
+          {error && <Error>{error}</Error>}
         </Content>
       }
       header="Buy Waifu"
       body="Select the number of Waifus that you would like to buy"
-      buttonAction={() => alert("Not implmented yet")}
+      buttonAction={() => {
+        validate();
+        alert("Not implmented yet");
+      }}
       buttonText="Buy"
     />
   );
