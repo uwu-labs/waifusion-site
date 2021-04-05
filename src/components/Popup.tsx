45fd874d106ec076c092914321847e38ac57bc96
Chase Manning <me@chasemanning.co.nz>
2021-04-05 14:43:11 +1200

    :sparkles: create popup component

---
+++ src/components/Popup.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledPopup = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 1;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  filter: blur(20px);
+`;
+
+const Container = styled.div`
+  position: relative;
+  width: 600px;
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: var(--plain);
+  border: 2px solid var(--plain-shadow);
+  box-shadow: 0 0.3rem 0 0 var(--plain-shadow);
+  font-size: 1.4rem;
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+type Props = {
+  show: boolean;
+  close: () => void;
+  content: JSX.Element;
+  header?: string;
+  buttonAction?: () => void;
+  buttonText?: string;
+};
+
+const Popup: React.FC<Props> = (props) => {
+  if (!props.show) return null;
+
+  return (
+    <StyledPopup>
+      <Background onClick={() => props.close()} />
+      <Container>{props.content}</Container>
+    </StyledPopup>
+  );
+};
+
+export default Popup;
