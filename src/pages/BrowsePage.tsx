019561823b4a8ad12fe07df25332e720567f5dd4
Chase Manning <me@chasemanning.co.nz>
2021-04-05 14:02:05 +1200

    :fire: remove alert popup

--- src/pages/BrowsePage.tsx
+++ src/pages/BrowsePage.tsx
@@ -69,7 +69,7 @@ const BrowsePage: React.FC = () => {
       body: null,
     });
     if (!response.success) {
-      alert(response.error?.code);
+      console.log(response.error?.code);
       return;
     }
     setPages(response.data.maxPage);
