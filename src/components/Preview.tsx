ac1660d93f869a3acb83d65fbec9db552e0ab952
Chase Manning <me@chasemanning.co.nz>
2021-04-11 10:56:39 +1200

    :rocket: add available waifus slide

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,44 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { ContractHelper } from "../services/contract";
+import GLOBALS from "../services/globals";
 import { Waifu } from "../types/waifusion";
 import WaifuCard from "./WaifuCard";
 
+const WAIU_COUNT = 5;
+
 const StyledPreview = styled.div`
   width: 100%;
-  padding: 3rem;
+  padding: 4rem;
+`;
+
+const Header = styled.h2`
+  color: var(--text-secondary);
+  font-size: 2rem;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 2rem;
 `;
 
 const WaifuWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-
-  > div {
-    margin-right: 1rem;
-  }
+  position: relative;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  grid-row-gap: 2rem;
+  margin: 0 3rem;
 `;
 
-const previewWaifusTemp: Waifu[] = [
-  {
-    id: 2043,
-    name: "Kaitlyn",
-  },
-  {
-    id: 5458,
-    name: "Tabby Catgirl",
-  },
-  {
-    id: 6941,
-    name: undefined,
-  },
-];
-
 const Preview: React.FC = () => {
+  const [waifus, setWaifus] = useState<Waifu[]>([]);
+
+  const getDungeonPreview = async () => {
+    const _waifus: Waifu[] = [];
+    const dungeonAddress = GLOBALS.DUNGEON_CONTRACT_ADDRESS;
+    const contractHelper = new ContractHelper();
+    await contractHelper.init();
+
+    const count = await contractHelper.waifuBalanceOfAddress(dungeonAddress);
+
+    await Array.from(Array(10).keys()).forEach(async (i: number) => {
+      const index = Math.floor(Math.random() * count);
+      const id = await contractHelper.tokenOfAddressByIndex(
+        index,
+        dungeonAddress
+      );
+      _waifus.push({ id });
+    });
+    console.log(_waifus);
+    setWaifus(_waifus);
+  };
+
+  useEffect(() => {
+    getDungeonPreview();
+  }, []);
+
   return (
     <StyledPreview>
-      This could be an example of some waifus maybe? Recently sold ones or
-      something.
+      <Header>Available Waifus</Header>
       <WaifuWrapper>
-        {previewWaifusTemp.map((waifu) => (
+        {waifus.map((waifu: Waifu) => (
           <WaifuCard key={waifu.id} waifu={waifu} />
         ))}
       </WaifuWrapper>
