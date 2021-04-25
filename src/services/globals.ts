aca48fcdd6d4dc7666e475061b7ff13af7712fba
Chase Manning <me@chasemanning.co.nz>
2021-04-25 11:18:12 +1000

    :fire: remove redundant name change price variable

--- src/services/globals.ts
+++ src/services/globals.ts
@@ -9,7 +9,6 @@ type Globals = {
   NFTX_WRAPPER: string;
   STARTING_INDEX: number;
   TOTAL_WAIFUS: number;
-  NAME_CHANGE_PRICE: number;
   APPROVE_AMOUNT: string;
   BUY_PRICE: string;
   CURRENCY: string;
@@ -46,7 +45,6 @@ const GLOBALS: Globals = {
     : "0xa862351f459ec386aa23e752d5435d268de2ef04",
   STARTING_INDEX: waifuVersion === "eth" ? 11595 : 11936,
   TOTAL_WAIFUS: 16384,
-  NAME_CHANGE_PRICE: 1830000000000000000000,
   APPROVE_AMOUNT: "16470000000000000000000",
   BUY_PRICE: TEST_MODE ? "0.07" : waifuVersion === "eth" ? "0.7" : "1.8",
   CURRENCY: waifuVersion === "eth" ? "ETH" : "BNB",
