0bf42593ad6a64193087af363ee6b84a63fce1f6
Chase Manning <me@chasemanning.co.nz>
2021-04-30 09:38:39 +1000

    :sparkles: add max option to input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  position: relative;
+`;
+
 const StyledInput = styled.input`
   border-radius: 1rem;
   border: 2px solid var(--plain-shadow);
@@ -16,21 +20,45 @@ const StyledInput = styled.input`
   }
 `;
 
+const Max = styled.button`
+  font-size: 1rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  color: var(--text-secondary);
+  cursor: pointer;
+  padding: 0.5rem;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 0.5rem;
+  border: none;
+  margin-top: 0;
+  background-color: var(--plain);
+`;
+
 type Props = {
   value: string;
   update: (value: string) => void;
   placeholder?: string;
   type?: string;
+  max?: string;
 };
 
 const Input: React.FC<Props> = (props) => {
   return (
-    <StyledInput
-      value={props.value}
-      type={props.type || "text"}
-      placeholder={props.placeholder}
-      onChange={(event) => props.update(event.target.value)}
-    />
+    <Container>
+      <StyledInput
+        value={props.value}
+        type={props.type || "text"}
+        placeholder={props.placeholder}
+        onChange={(event) => props.update(event.target.value)}
+      />
+      {props.max && <Max onClick={() => props.update(props.max!)}>max</Max>}
+    </Container>
   );
 };
 
