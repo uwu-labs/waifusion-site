8a1d6331951e424a5821c59b9155a6f550d51649
Denis <dsapaiv@gmail.com>
2021-08-07 14:31:19 +0300

    404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,7 +4,28 @@ import styled from "styled-components";
 import Head from "../components/Head";
 import Header from "../components/Header";
 
-const StyledNotFoundPage = styled.div``;
+const StyledNotFoundPage = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledNotFoundText = styled.h1`
+  background: linear-gradient(
+      45deg,
+      var(--primary) 0%,
+      var(--secondary) 25%,
+      var(--highlight) 50%,
+      var(--secondary) 75%,
+      var(--primary) 100%
+    )
+    0% 0% / 300% 300%;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-animation: ZMLIY 3s linear 0s infinite;
+  animation: ZMLIY 3s linear 0s infinite;
+`;
 
 const NotFoundPage: React.FC = () => {
   const [t] = useTranslation();
@@ -12,7 +33,7 @@ const NotFoundPage: React.FC = () => {
   return (
     <StyledNotFoundPage>
       <Head title={t("headers.notFound")} />
-      <Header text={t("headers.notFound")} />
+      <StyledNotFoundText>{t("headers.notFound")}</StyledNotFoundText>
     </StyledNotFoundPage>
   );
 };
