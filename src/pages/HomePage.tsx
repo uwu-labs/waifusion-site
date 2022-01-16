1d93e2f02506c73c52b1bdbccf626db5e363e971
Chase Manning <me@chasemanning.co.nz>
2022-01-16 11:21:36 +0200

    :rocket: hide preview on eth

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import Head from "../components/Head";
 import Landing from "../components/Landing";
 import Preview from "../components/Preview";
 import WetSlide from "../components/WetSlide";
+import { selectIsEth } from "../state/reducers/globals";
 
 const Container = styled.div`
   width: 100%;
@@ -12,12 +14,14 @@ const Container = styled.div`
 `;
 
 const Home: React.FC = () => {
+  const isEth = useSelector(selectIsEth);
+
   return (
     <Container>
       <Head title="Anime Ethereum NFT Collectibles" />
       <Landing />
       <WetSlide />
-      <Preview />
+      {!isEth && <Preview />}
     </Container>
   );
 };
